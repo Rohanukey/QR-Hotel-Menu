788fbf0d27245cff7905e9f371b8276517e7c0d2
Rohanukey <153976817+Rohanukey@users.noreply.github.com>
2024-04-06 13:10:19 +0530

    updated 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-// import { useState } from 'react'
-import CategoryPage from './Component/Category/Category';
 
-
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Wrapper from './Component/Wrapper/Wrapper'
+import AddOns from './Component/AddOns/AddOns'
+import Dashboard from "./comonent/Dashboard";
+import Header from "./comonent/Header";
+import Cart from "./comonent/Cart";
+import MergeHeader from './Component/MergeHeader/MergeHeader';
+import Test from './DeffComponenet/ProductForm/ProductForm';
+import Category from "./Component/Category/Category"
 
 function App() {
 
   return (
     <>
-     <CategoryPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Wrapper />} />
+          <Route path='/Addons' element={<AddOns />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/H" element={<Header/>} />
+          <Route path="/MenuPage" element={<MergeHeader/>} />
+          <Route path="/Test" element={<Test/>} />
+          <Route path="/Caterory" element={<Category/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
